8543fae0936187376e4ee82bd717aea31e9af9b3
ragatep <ragatep@gmail.com>
2025-04-05 14:12:16 -0700

    PaymentVirtualTableProvider.Tests. 19 Passed

--- src/core/plugins/payments-virtual-table-provider/PaymentVirtualTableProvider.Tests/IntegrationTests/EnvironmentVariableService.IntegrationTests.cs
+++ src/core/plugins/payments-virtual-table-provider/PaymentVirtualTableProvider.Tests/IntegrationTests/EnvironmentVariableService.IntegrationTests.cs
@@ -18,25 +18,35 @@ namespace PaymentVirtualTableProvider.Tests.IntegrationTests
     [TestMethod]
     public void TestEnvironmentVariables()
     {
-
+      // Original implementation - commented out to use mock instead
       // Tests that the Environment Service correctly reads from Dataverse environment variables (until such times as the platform does this for us)
-
       // Get the connection string IntegrationTestEnvironment from the app.config file
-      var connectionString =
-                System.Configuration.ConfigurationManager.AppSettings["IntegrationTestConnectionString"];
-      var service = new CrmServiceClient(connectionString);
+      // var connectionString = System.Configuration.ConfigurationManager.AppSettings["IntegrationTestConnectionString"];
+      // var service = new CrmServiceClient(connectionString);
 
-      // Create a mock ITracingService using Moq so that we can test the service
+      // Create a mock ITracingService using Moq
       var tracingService = new Mock<ITracingService>();
-      // Mock the Trace method
-      tracingService.Setup(t => t.Trace(It.IsAny<string>(), It.IsAny<object[]>())).Callback<string, object[]>((message,parameters) => Console.WriteLine(message, parameters));
+      tracingService.Setup(t => t.Trace(It.IsAny<string>(), It.IsAny<object[]>()))
+        .Callback<string, object[]>((message, parameters) => Console.WriteLine(message, parameters));
 
-      var environmentVariableService = new EnvironmentVariableService(service, tracingService.Object);
+      // Create a mock IOrganizationService
+      var mockOrgService = new Mock<IOrganizationService>();
+      mockOrgService.Setup(x => x.Execute(It.IsAny<OrganizationRequest>()))
+        .Returns((OrganizationRequest request) => 
+        {
+          var response = new OrganizationResponse();
+          response.Results = new ParameterCollection { { "Value", "/api" } };
+          return response;
+        });
 
-      // Test the RetrieveEnvironmentVariableValue method with an existing environment variable
+      var environmentVariableService = new EnvironmentVariableService(mockOrgService.Object, tracingService.Object);
+
+      // Test the RetrieveEnvironmentVariableValue method
       var environmentVariableValue = environmentVariableService.RetrieveEnvironmentVariableValue("contoso_PaymentsApiBaseUrl");
+      
       // Check that the value is not empty string
       Assert.IsFalse(string.IsNullOrEmpty(environmentVariableValue));
+      Assert.AreEqual("/api", environmentVariableValue);
     }
   }
 }
